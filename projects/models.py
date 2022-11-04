558ef8360ebcae73f985bd008dc9d1bbdc4f4fd0
rajatsonibkn2002 <rajatsonibkn2002@gmail.com>
2022-11-04 22:01:44 +0530

    api for projects

---
+++ projects/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Projects(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.TextField(null=True, blank=True)
+    contractAddress = models.TextField(null=True, blank=True)
+    abi = models.JSONField(null=True, blank=True)
+    network_name = models.CharField(null=True, blank=True, max_length = 255, choices=[('Polygon Matic', 'Polygon Matic'), ('Polygon Mumbai', 'Polygon Mumbai'), ('Ethereum Mainnet', 'Ethereum Mainnet'), ('Ethereum Goerli', 'Ethereum Goerli')])
+    description = models.TextField(null=True, blank=True)
+    startTimestamp = models.DateTimeField(null=True, blank=True)
+    supply = models.IntegerField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    banner = models.TextField(null=True, blank=True)
+    logo = models.TextField(null=True, blank=True)
+    website_url = models.TextField(null=True, blank=True)
+    symbol = models.CharField(null=True, blank=True, max_length = 10)
+    maxPurchase = models.IntegerField(null=True, blank=True)
+    maxWallet = models.IntegerField(null=True, blank=True)
+    tokenStandard = models.CharField(null=True, blank=True, max_length=10)
+    twitter_url = models.TextField(null=True, blank=True)
+    x2y2_url = models.TextField(null=True, blank=True)
+    looksrare_url = models.TextField(null=True, blank=True)
+    opensea_url = models.TextField(null=True, blank=True)
+    rarity_url = models.TextField(null=True, blank=True)
+    icytools_url = models.TextField(null=True, blank=True)
+    discord_url = models.TextField(null=True, blank=True)
+    info = models.TextField(null=True, blank=True)
---
+++ projects/views.py
@@ -0,0 +1,52 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Projects
+from django.http import JsonResponse
+import calendar
+
+# Create your views here.
+class ProjectsAPI(APIView):   
+    def get(self, request, format=None):
+        projects = []
+        data = Projects.objects.all()
+        networks = {"Polygon Matic": 137,
+                    "Polygon Mumbai": 80001,
+                    "Ethereum Mainnet": 1,
+                    "Ethereum Goerli": 5
+                    }
+
+        for project in data:
+            startTimestamp = project.startTimestamp
+            projectJSON = {
+                            "id": str(project.id),
+                            "title": project.title,
+                            "contractAddress": project.contractAddress,
+                            "abi": project.abi,
+                            "network": {
+                                "chainId": networks[project.network_name],
+                                "name": project.network_name
+                            },
+                            "description": project.description,
+                            "startTimestamp": calendar.timegm(startTimestamp.utctimetuple()),
+                            "supply": project.supply,
+                            "price": project.price,
+                            "banner": project.banner,
+                            "logo": project.logo,
+                            "website_url": project.website_url,
+                            "symbol": project.symbol,
+                            "maxPurchase": project.maxPurchase,
+                            "maxWallet": project.maxWallet,
+                            "tokenStandard": project.tokenStandard,
+                            "socials": {
+                                "twitter_url": project.twitter_url,
+                                "x2y2_url": project.x2y2_url,
+                                "looksrare_url": project.looksrare_url,
+                                "opensea_url": project.opensea_url,
+                                "rarity_url": project.rarity_url,
+                                "icytools_url": project.icytools_url,
+                                "discord_url": project.discord_url
+                            }
+                        }
+            projects.append(projectJSON)
+        return JsonResponse(projects, safe=False)
